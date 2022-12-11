f7d83aff0d92645eeb1ffeeb1847c0a7ac14f356
HoaiHuong1998 <hoaihuong401@gmail.com>
2022-12-11 14:37:18 +0700

    abcd

--- src/count/count-title/CountTitle.js
+++
@@ -1,9 +0,0 @@
-import '../../base-title/BaseTitle.css'
-import BaseTitle from "../../base-title/BaseTitle"
-import React from 'react'
-
-export default class CountTitle extends React.Component {
-    render() {
-        return <BaseTitle name='退勤統計'　/>
-    }
-}
